10747da2b881a7888904c3e874280f473d8c205e
David <lairdavid1@gmail.com>
2023-10-08 23:35:55 -0400

    Started implementing ARIMA

--- Case1/main.py
+++ Case1/main.py
@@ -2,6 +2,8 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from collections import defaultdict
 import math
 import matplotlib.pyplot as plt
@@ -39,6 +41,7 @@ def get_data(portfolio, start_date, end_date):
     data.columns = pd.MultiIndex.from_tuples(columns, names=['country', 'attribute', 'ticker'])
     return data
 
+
 def momentum_trading_strat(portfolio, start_date, end_date):
     data = get_data(portfolio, start_date, end_date)
     trading_list = pd.DataFrame(columns = data.columns.levels[2])
@@ -48,30 +51,27 @@ def momentum_trading_strat(portfolio, start_date, end_date):
             returns = data[country]['Adj Close'][ticker]
 
             # Convert closing prices to pandas series for easier manipulation
-            price_series = pd.Series(returns.values.tolist(), index=returns.index)
+            price_series = pd.Series(returns.values.tolist(), index=returns.index.tolist())
+            trading_list[ticker] = price_series
+    return trading_list
 
-            # Number of periods for SMA 
-            span_period = 5
+def predict_data(dataframe):
+    results = arima(dataframe)
+    return results.forecast()
 
-            # Calculate SMA
-            sma = price_series.rolling(window=span_period, min_periods=1).mean()
-            
-            ticker_trades = pd.DataFrame(columns = [ticker])
-            for date in returns.index:
-                if (returns[date] > sma[date]):
-                    ticker_trades.loc[date] = 1
-                elif (returns[date] < sma[date]):
-                    ticker_trades.loc[date] = -1
-                else:
-                    # TODO: Set to 0 but do not get divide by zero error
-                    ticker_trades.loc[date] = -1
-    trading_list = pd.concat([trading_list, ticker_trades], axis=1)
-    trading_list_one = trading_list.sub(trading_list.mean(axis=1), axis=0)
-    trading_list_norm = trading_list_one.div(trading_list_one.abs().sum(axis=1), axis=0)
-    return trading_list_norma
+def arima(dataframe):
+    p = 1
+    d = 1
+    q = 1
+    model = ARIMA(dataframe, order=(p, d, q))
+    results = model.fit()
+    return results
 
 trading_list = momentum_trading_strat(portfolio, '2015-01-01', '2023-01-01')
 print(trading_list)
 
+results = arima(trading_list['AAPL'])
+forecast = results.forecast()
+print(forecast)
 
 
